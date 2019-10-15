50778c609250475d06ccd8a8e2c515f7e6a82827
Leyla Tarhan <lytarhan@g.harvard.edu>
2019-10-14 21:33:26 -0400

    Added in details for sparse network code

--- 1-replicability/Step2_makeSparseNetwork.m
+++ 1-replicability/Step2_makeSparseNetwork.m
@@ -11,8 +11,7 @@
 % the alternative is a fully-connected network, which takes longer to
 % compute because the optimizer has to consider more possible paths. 
 
-% input: voxels x voxels matrix containing the real distances between every
-% pair of voxels (e.g., in mm). 
+% input: coordinates for every voxel (i, j, k)
 % output: a list of direct connections between downsampled meta-voxels. 
 
 % To use this script: download the dataset "1-Replicability" from this project's OSF
@@ -37,11 +36,18 @@ addpath('../utils')
 
 %% Setup
 
-nLocalEdges = 6; % how many local connections to make in the first step
 origVoxSize = 3.0; % how large are voxels in each dimension? (assumes isotropic)
+localMaxDist = 3.0; % max distance for connecting "local" voxels in step 1
+% default: distance between voxels
 
-pAddEdges = 1; % [] 
-% specify the probability for adding edges during step 2
+
+allowed_inflation = 1.01; % parameter to decide when to delete edges 
+% -- if 3 voxels are all directly connected, and the sum of 2 edges <= this factor * 3rd edge, delete the 3rd edge
+
+distRatioStability = 0.01; % parameter to decide whether the max distance 
+% ratio is "stable" between iterations of refining the network -- stop 
+% when the current max distance ratio - the last iteration's max distance 
+% ratio is <= this value
 maxDistRatio = 1.1; % max distance ratio (captures the differences between 
 % the sparse and fully-connected networks). Use this parameter to decide
 % when the sparse network is done.
@@ -62,45 +68,81 @@ disp('made meta-voxels.')
 
 %% step 1: initialize the network
 
-% ...initialize a totally unconnected graph (# vertices = # voxels, but 0
+% ...initialize a totally unconnected graph (# vertices = # meta-voxels, but 0
 % edges)
+% [] sparse matrix (meta-voxels x meta-voxels):
+    % ...1 = voxels are connected
+    % ...0 = voxels aren't connected
 
 % ...add in local connections
-    % ...connect every voxel with its 6 nearest neighbors
+    % ...connect every voxel with its nearest neighbors (defined by some
+    % max distance)
+    % [] get the cells in the n x n distance matrix <= localMaxDist, update the corresponding entries of the sparse network matrix 
     
 
 %% step 2: add edges
 
 % ...calculate the distance ratio for each pair of voxels
     % ...graph-distance / actual brain distance
-    % ...1 edge in graph-space = length of 1 (?)
+    % ...length of each edge = actual brain distance between the connected voxels
     % ...if the value of the distance ratio is much greater than 1, the
     % sparse network is much less efficient than the fully-connected one
     % (you have to take a circuitous path to get between nearby voxels)
+    % [] find a function like floyd_warshall_shortest_paths that find the
+    % shortest path between voxels
     
 % ...add an edge between each pair of meta-voxels according to some
-% probability function
-% [] specify the probability function
+% probability function (IF they're not already directly connected)
+        % ...if distance ratio <= 1.05, probability of adding
+        % an edge = 0 (don't add an edge)
+        % ...if distance ratio > 1.05, probability of adding an edge =
+        % distance ratio - 1.05 (could be > 1)
+        % ...decide whether to add this edge:
+            % ...generate a random # [0, 1]. If that random # is < the probability of adding an edge, add the edge. Otherwise, don't add it. 
+        
 % ...voxel pairs with higher distance ratios are more likely to be
 % connected by a new edge
-% [] update the distance ratios for all the voxel pairs
+
 
 %% step 3: trim edges
 
 % ...look at every "triangle" of meta-voxels (set of 3 voxels that are all
 % directly connected)
+    % ...go through the upper triangle of the network matrix (all i's are <
+    % j)
+        % ...multiply rows i & j 
+        % ...look for 1's in the product (i & j were connected to k)
+            % ...only look in indices >= j+1 to avoid finding the same
+            % triangle multiple times
     % ...for each edge, if sum of the other 2 edges ~ that edge, delete
-    % that edge
+    % that edge 
+            % ...edge1 + edge2 < allowed_inflation*edge 3 --> delete edge 3
+    
+% [] update distance ratios again
     
 %% step 4: refine
 
 % [] repeat steps 2 & 3 until the max distance ratio becomes stable
-% [] increase probability of adding an edge
+% [] check max distance ratio (is it within the bounds we defined above?)
 
+% switch to adding edges more than trimming edges
+% [] increase probability of adding an edge -- few ways to do this:
+    % ...run adding step a few times in a row without the trimming step
+    % ...increase the probabilities (multiply them all by some factor)
+    % ...decrease cutting probabilities somehow?
+% [] check max distance ratio again
+    
 % [] go through steps 2 & 3 a few more times, increasing the probability 
 % of adding an edge after each iteration
-% ...stop when the sparse network is reasonably similar to the
+% ...stop when the sparse network's efficiency is reasonably similar to the
 % fully-connected network
+
+%% save the sparse network
+
+% format: matrix, where each row = 1 edge (col 1 = voxel 1; col 2 = voxel
+% 2)
+
+% [] save as a .csv or .mat file
 
 
     
